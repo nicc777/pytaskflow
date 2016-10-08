d025a210bda4fec3f48fbb381ef9fc03da6dfb9c
Nicolaas Coetzee <info@oculusd.com>
2016-10-08 16:13:28 +0200

    Added WorkFlow unittest

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,5 +1,5 @@
 import unittest
-from pytaskflow.taskflow_engine import Function, Result, Task
+from pytaskflow.taskflow_engine import Function, Result, Task, WorkFlow
 
 # the inclusion of the tests module is not meant to offer best practices for
 # testing in general, but rather to support the `find_packages` example in
@@ -48,6 +48,20 @@ class TaskTests(unittest.TestCase):
         t1.run_task(input_result=Result(result_obj={}))
         self.assertEqual(t1.task_result.result_obj['Message'], 'Hello World')
         self.assertEqual(t1.task_result.result_obj['Variable'], 10)
+
+
+class WorkFlowTests(unittest.TestCase):
+    def test_basic_workflow_positive001(self):
+        f1 = PrintFunction()
+        f2 = SetVariableFunction()
+        t1 = Task(task_name='Task 1')
+        t2 = Task(task_name='Task 2')
+        t2.register_function(function=f1, success_task=None, err_task=None)
+        t1.register_function(function=f2, success_task=t2, err_task=None)
+        wf = WorkFlow(workflow_name='Workflow Test', starter_task=t1)
+        result = wf.run_workflow(input_result=Result(result_obj={}))
+        self.assertEqual(result.result_obj['Message'], 'Hello World')
+        self.assertEqual(result.result_obj['Variable'], 10)
 
 
 if __name__ == '__main__':
